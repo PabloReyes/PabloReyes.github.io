58ea2a200e2b960f080979720320987317be310c
Pablo Reyes <reyes@strands.com>
2017-02-04 18:24:02 +0100

    New article. Articles list is back. Styling.

--- js/main.js
+++ js/main.js
@@ -8,4 +8,14 @@ $(document).ready(function () {
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
+    $('.navigation--blog').click(function(e){
+      e.preventDefault();
+      var panel = $('.panel-cover');
+      if(panel.hasClass('panel-cover--collapsed')){
+          panel.removeClass('panel-cover--collapsed');
+      }
+      else{
+          panel.addClass('panel-cover--collapsed');
+      }
+    });
 })
