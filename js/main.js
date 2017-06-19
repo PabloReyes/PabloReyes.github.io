2300d39ea15839144f46213e41f5b3855c0c0c55
Pablo Reyes <reyes@strands.com>
2017-06-19 19:16:16 -0400

    Remove not needed JS code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
 ---
 layout: null
 ---
+
 $(document).ready(function () {
 
-  $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
-
-    $('.navigation--blog').click(function(e){
-      e.preventDefault();
-      var panel = $('.panel-cover');
-      if(panel.hasClass('panel-cover--collapsed')){
-          panel.removeClass('panel-cover--collapsed');
-      }
-      else{
-          panel.addClass('panel-cover--collapsed');
-      }
-    });
 })
